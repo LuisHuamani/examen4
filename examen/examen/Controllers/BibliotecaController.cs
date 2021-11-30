3b6d04cccb8e74f9fb7ff8baef4f20353f7de2ed
LuisHuamani <luis.huamani.c@tecsup.edu.pe>
2021-11-30 09:45:45 -0500

    'examen'

---
+++ examen/examen/Controllers/BibliotecaController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace examen.Controllers
+{
+    public class BibliotecaController : Controller
+    {
+        #region Contexto
+
+        private BibliotecaEntities _contexto;
+
+        public BibliotecaEntities contexto
+        {
+            set { _contexto = value; }
+            get
+            {
+                if (_contexto == null)
+                    _contexto = new BibliotecaEntities();
+                return _contexto;
+            }
+        }
+
+        // GET: Biblioteca
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
